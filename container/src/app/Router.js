e8fdbe796a45b1f5f21c9b2643d028de8411c8ba
didof <didonato.fr@gmail.com>
2021-06-11 18:48:25 +0200

    Fixed routing partially
    The Click on See All should update the rendering to All of the mfe
    products, but it provoke the cancellation of the mfe from the rendering
    since the super-route in the container gets triggered

--- container/src/app/Router.js
+++ container/src/app/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, useHistory } from 'react-router-dom'
 import Nav from '../components/Nav/Nav'
 
 import Home from '../pages/Home'
@@ -8,11 +8,36 @@ import Products from '../pages/Products'
 export default () => {
   return (
     <BrowserRouter>
+      <_Internal />
+    </BrowserRouter>
+  )
+}
+
+const _Internal = () => {
+  const history = useHistory()
+
+  const navigationHandle = event => {
+    const { pathname } = event.detail
+    if (pathname === history.location.pathname) return
+
+    history.push(pathname)
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('navigation', navigationHandle)
+    return () => {
+      window.removeEventListener('navigation', navigationHandle)
+    }
+  }, [])
+
+  return (
+    <>
       <Nav />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/products' component={Products} />
+        <Route path='/discount' component={Products} />
       </Switch>
-    </BrowserRouter>
+    </>
   )
 }
